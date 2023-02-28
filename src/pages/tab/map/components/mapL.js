7b2eb331f9c5cfec1abe0ed001a14a4917f40ad8
trieunq <trieunq@thomi.com.vn>
2023-02-28 09:25:53 +0700

    test map

--- src/pages/tab/map/components/mapL.js
+++ src/pages/tab/map/components/mapL.js
@@ -271,8 +271,8 @@ function MapL() {
                 }
             })
             // bắt sự kiện click để hiển thị vị trí hiện tại 
-            // let islocation = 0;
-            // let currentlocation;
+            let islocation = 0;
+            let currentlocation;
             // locationBtn.addEventListener('click', () => {
 
             //     // Notiflix.Loading.pulse();
@@ -301,29 +301,34 @@ function MapL() {
             //     //     islocation += 1
             //     // }
             // })
-            //Adds the map control and sets the center point to Durham and zoom scale to 10. For more info on the Leaflet map control check out: http://leafletjs.com/reference.html#map-class
-            const onLocationFound = (e) => {
-                var radius = e.accuracy / 2;
-
-                //Create pop-up to display location accuracy.  Location is coming from IP location or GPS receiver in mobile device
-                L.marker(e.latlng).addTo(map)
-                    .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-                //Create circle to graphically represent location accuracy
-                L.circle(e.latlng, radius).addTo(map);
-            }
-
-            //Code for handling any errors that come up
-            const onLocationError = (e) => {
-                alert(e.message);
-            }
-
-            map.on('locationfound', onLocationFound);
-            map.on('locationerror', onLocationError);
-
-            //Locate point and zoom to that location with a zoom level of 16
-            map.locate({ setView: true, maxZoom: 16 });
             L.control.locate().addTo(map);
+            map.on('locationfound', function (e) {
+                Notiflix.Loading.pulse();
+                if (islocation == 1) {
+                    map.removeLayer(currentlocation).setView([11.534428, 107.129069], 17)
+                    locationBtn.style.background = '#fff'
+                    locationBtn.style.color = '#000'
+                    islocation = 0
+                    Notiflix.Loading.remove();
+                } else {
+                    // Lấy vị trí hiện tại của thiết bị
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        var pos = [position.coords.latitude, position.coords.longitude];
+                        map.setView(pos, 19);
+                        var customIcon = L.icon({
+                            iconUrl: locationIcon,
+                            iconSize: [17, 17], // kích thước của icon
+                            iconAnchor: [8.5, 8.5], // điểm neo của icon
+                            popupAnchor: [0, -8.5] // vị trí hiển thị popup
+                        });
+                        currentlocation = L.marker(pos, { icon: customIcon }).addTo(map)
+                        Notiflix.Loading.remove();
+                    });
+                    locationBtn.style.background = '#6AB100'
+                    locationBtn.style.color = '#fff'
+                    islocation += 1
+                }
+            });
         }
 
     }, [datacay])
